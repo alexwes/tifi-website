6529bfb52a32c44cdfab9e1488ede91d4556824f
Alex Weseley <pampawes@gmail.com>
2023-10-16 10:10:27 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+// <!-- Source: https://www.w3schools.com/howto/howto_js_slideshow.asp -->
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+      slideIndex = 1;
+    }
+  if (n < 1) {
+      slideIndex = slides.length;
+    }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+
+}
+
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  if (n > slides2.length) {
+      slideIndex2 = 1;
+    }
+  if (n < 1) {
+      slideIndex2 = slides2.length;
+    }
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+  slides2[slideIndex2-1].style.display = "block";
+}
+
+// Source: https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+function hamburger() {
+  var x = document.getElementById("navbar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
